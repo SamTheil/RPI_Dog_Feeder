a1a4885fcf5a35186efa06999d8618b7062457ea
SamTheil <137443849+SamTheil@users.noreply.github.com>
2024-06-27 10:07:44 -0400

    fix enable/disable access poin

--- accessPoint_enable.sh
+++ accessPoint_enable.sh
@@ -1,4 +1,10 @@
-sudo \cp /etc/dhcpcd.accesspoint.conf /etc/dhcpcd.conf;
-sudo systemctl enable hostapd;
-sudo systemctl enable dnsmasq;
-sudo reboot;
+#!/bin/bash
+sudo \cp /etc/dhcpcd.accesspoint.conf /etc/dhcpcd.conf
+sudo systemctl stop wpa_supplicant
+sudo systemctl disable wpa_supplicant
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+sudo systemctl start hostapd
+sudo systemctl enable dnsmasq
+sudo systemctl start dnsmasq
+sudo reboot
