e6080ba564db837f8986004676973e433e2c9834
SamTheil <137443849+SamTheil@users.noreply.github.com>
2024-06-27 12:01:52 -0400

    revert

--- setup.sh
+++ setup.sh
@@ -75,72 +75,11 @@ systemctl enable flaskapp.service
 echo "Starting Flask app service..."
 systemctl start flaskapp.service
 
-sudo apt-get install hostapd dnsmasq
-
 # Check the status of the Flask app service
 systemctl status flaskapp.service --no-pager
 
 # Reload systemd manager configuration
 echo "Reloading systemd manager configuration..."
 systemctl daemon-reload
-
-sudo tee /etc/dhcpcd.orig.conf << 'EOF'
-# Original dhcpcd.conf content
-# Example configuration:
-
-hostname
-clientid
-persistent
-option rapid_commit
-option domain_name_servers, domain_name, domain_search, host_name
-option classless_static_routes
-option interface_mtu
-require dhcp_server_identifier
-slaac private
-# Additional original configurations...
-EOF
-
-sudo tee /etc/dhcpcd.accesspoint.conf << 'EOF'
-# Modified dhcpcd.conf content for access point
-hostname
-clientid
-persistent
-option rapid_commit
-option domain_name_servers, domain_name, domain_search, host_name
-option classless_static_routes
-option interface_mtu
-require dhcp_server_identifier
-slaac private
-
-# Static IP configuration for access point
-interface wlan0
-    static ip_address=192.168.4.1/24
-    nohook wpa_supplicant
-# Additional access point configurations...
-EOF
-
-sudo tee /etc/hostapd/hostapd.conf << 'EOF'
-interface=wlan0
-driver=nl80211
-ssid=dogfeeder
-hw_mode=g
-channel=7
-wmm_enabled=0
-macaddr_acl=0
-auth_algs=1
-ignore_broadcast_ssid=0
-wpa=2
-wpa_passphrase=password
-wpa_key_mgmt=WPA-PSK
-rsn_pairwise=CCMP
-EOF
-
-sudo tee /etc/dnsmasq.conf << 'EOF'
-interface=wlan0
-dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h
-EOF
-
-sudo sed -i 's|#DAEMON_CONF="|DAEMON_CONF="/etc/hostapd/hostapd.conf|' /etc/default/hostapd
-
 
 echo "Setup complete. You can now access your Flask app using http://feeder.local (after running the app)."
