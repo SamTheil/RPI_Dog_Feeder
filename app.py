3f066929a5153fc2ffcc797adaa00bc4171e0f2f
SamTheil <137443849+SamTheil@users.noreply.github.com>
2024-07-04 09:56:27 -0400

    revert

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import os
+import time
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/change_wifi', methods=['POST'])
+def change_wifi():
+    ssid = request.form['ssid']
+    password = request.form['password']
+    
+    # Create a wpa_supplicant.conf content
+    wpa_supplicant_conf = f"""
+    country=US
+    ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+    update_config=1
+
+    network={{
+        ssid="{ssid}"
+        psk="{password}"
+    }}
+    """
+
+    # Write the new configuration to wpa_supplicant.conf
+    with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'w') as f:
+        f.write(wpa_supplicant_conf)
+
+    # Restart the WiFi interface
+    os.system('sudo wpa_cli -i wlan0 reconfigure')
+
+    flash('WiFi settings have been changed. Device will reboot now.')
+
+    time.sleep(1)
+
+    os.system('sudo reboot')
+
+@app.route('/enable_ap', methods=['POST'])
+def enable_ap():
+    os.system('chmod +x accessPoint_enable.sh')
+    os.system('sudo ./accessPoint_enable.sh')
+    flash('Access Point enabled. Device will reboot now.')
+    time.sleep(1)
+    return redirect(url_for('home'))
+
+@app.route('/disable_ap', methods=['POST'])
+def disable_ap():
+    os.system('chmod +x accessPoint_disable.sh')
+    os.system('sudo ./accessPoint_disable.sh')
+    flash('Access Point disabled. Device will reboot now.')
+    time.sleep(1)
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
